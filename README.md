5d0afb6c79f1b72c62b69ee518233cffd77969fc
Philippe Marschall <philippe.marschall@gmail.com>
2018-05-23 10:23:44 +0200

    actually execute query

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-http://127.0.0.1:8080/query-timeout/tx
-http://127.0.0.1:8080/query-timeout/no-tx
+Test for verifications that the transaction timeout is properly propagated to the query timeout
+
+The application can be found under:
+
+* http://127.0.0.1:8080/query-timeout/tx
+* http://127.0.0.1:8080/query-timeout/no-tx
--- src/main/java/com/github/marschall/transactionquerytimeouttest/QueryTimeoutUtil.java
+++ src/main/java/com/github/marschall/transactionquerytimeouttest/QueryTimeoutUtil.java
@@ -2,6 +2,7 @@ package com.github.marschall.transactionquerytimeouttest;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Objects;
 
@@ -19,6 +20,11 @@ final class QueryTimeoutUtil {
   int getQueryTimeout() {
     try (Connection connection = this.dataSource.getConnection();
          PreparedStatement statement = connection.prepareStatement("SELECT 1 FROM dual")) {
+      try (ResultSet resultSet = statement.executeQuery()) {
+        while (resultSet.next()) {
+          resultSet.getInt(1);
+        }
+      }
       return statement.getQueryTimeout();
     } catch (SQLException e) {
       throw new RuntimeException("could not get query timeout", e);
